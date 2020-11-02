3897b0f434b590436eb12e53080cffa32d8a2edf
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-11-02 16:56:47 +0200

    added google maps client

--- client/src/containers/DashBoard.tsx
+++ client/src/containers/DashBoard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Interpreter } from "xstate";
+import axios from "axios";
+import GoogleMapsTile from "../components/GoogleMapsTile";
 import { AuthMachineContext, AuthMachineEvents } from "../machines/authMachine";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 export interface Props {
   authService: Interpreter<AuthMachineContext, any, AuthMachineEvents, any>;
@@ -9,6 +12,9 @@ export interface Props {
 const DashBoard: React.FC = () => {
   return (
     <>
+      <ErrorBoundry>
+        <GoogleMapsTile />
+      </ErrorBoundry>
     </>
   );
 };
